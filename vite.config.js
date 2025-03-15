d9081a97dbda85e6e46420563257a74ddff27467
RafanSTR <153357481+RafanSTR@users.noreply.github.com>
2025-03-15 13:43:59 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  base: "/pages/",  // Ganti dengan nama repository GitHub kamu
 });
